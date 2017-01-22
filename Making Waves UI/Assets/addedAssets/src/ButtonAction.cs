cd252e929b45507a0e699b7f04d6c05f529fdcce
Nelson Schoenbrot <schoenbrot@Nelsons-MacBook-Pro.local>
2017-01-22 14:00:35 -0500

    Added script to button.

---
+++ Making Waves UI/Assets/addedAssets/src/ButtonAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour {
+
+	public void launchGame() {
+		Debug.Log("Attempting to launch the game.");
+	}
+
+	public void launchInstrcutions() {
+		Debug.Log ("Attempting to launch the instructions.");
+	}
+}
